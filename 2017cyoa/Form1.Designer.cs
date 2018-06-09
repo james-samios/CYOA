d069065793526b32ffab7beaec81759fce9c7b88
James Samios <james@samios.io>
2018-06-09 10:09:59 +1000

    Initial commit after SSD Failure

---
+++ 2017cyoa/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace _2017cyoa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStory = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblStory
+            // 
+            this.lblStory.Location = new System.Drawing.Point(12, 9);
+            this.lblStory.Name = "lblStory";
+            this.lblStory.Size = new System.Drawing.Size(389, 314);
+            this.lblStory.TabIndex = 0;
+            this.lblStory.Text = "label1";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(15, 326);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(386, 96);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "button1";
+            this.btn1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(15, 428);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(386, 93);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "button2";
+            this.btn2.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(12, 527);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(386, 96);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "button3";
+            this.btn3.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 603);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lblStory);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStory;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+    }
+}
+
---
+++ 2017cyoa/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2017cyoa
+{
+
+    public partial class Form1 : Form
+    {
+        // We create our own data type here to store the
+        // content of the file as we read it in. 
+        public struct Room
+        {
+            public int roomID;
+            public string story;
+            public int opt1;
+            public string opt1Text;
+            public int opt2;
+            public string opt2Text;
+            public int opt3;
+            public string opt3Text;
+        }
+
+        // Create an array of our new data type.
+        // remember to change the value in the [] 
+        // to the number of lines in your text file
+        Room[] room = new Room[2]; 
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // add the other buttons to the same event
+            this.btn2.Click += btn1_Click;
+            this.btn3.Click += btn1_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // load the file into memory
+            readFile();
+
+            // load the initial room
+            loadRoom(0);
+        }
+
+        private void readFile()
+        {
+            string line;
+
+            // indicates the room in the array
+            int counter = 0;
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file =
+               new System.IO.StreamReader(Application.StartupPath + "\\story.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                // divide the string on the | character
+                // and store in a temporary array
+                string[] tmp = line.Split('|');
+
+                // load the string into the structure
+                room[counter].roomID = int.Parse(tmp[0]);
+                room[counter].story = tmp[1];
+                room[counter].opt1 = int.Parse(tmp[2]);
+                room[counter].opt1Text = tmp[3];
+                room[counter].opt2 = int.Parse(tmp[4]);
+                room[counter].opt2Text = tmp[5];
+                room[counter].opt3 = int.Parse(tmp[6]);
+                room[counter].opt3Text = tmp[7];
+                counter++;
+            }
+
+            file.Close();
+        }
+
+        // This function loads a room from the array
+        // of rooms into the controls on the screen.
+        private void loadRoom(int roomID)
+        {
+            lblStory.Text = room[roomID].story;
+            btn1.Text = room[roomID].opt1Text;
+
+            // store the target room for the button click
+            btn1.Tag = room[roomID].opt1;
+            btn2.Text = room[roomID].opt2Text;
+            btn2.Tag = room[roomID].opt2;
+            btn3.Text = room[roomID].opt3Text;
+            btn3.Tag = room[roomID].opt3;
+        }
+
+        // This function handles the click button event
+        // for all the controls on the screen
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            // Cast the 'sender' as a button
+            Button sentButton = (Button)sender;
+
+            // Extract the target room of the button
+            int ID = int.Parse(sentButton.Tag.ToString());
+
+            // Call the loadRoom function and pass the ID
+            loadRoom(ID);
+        }
+    }
+}
