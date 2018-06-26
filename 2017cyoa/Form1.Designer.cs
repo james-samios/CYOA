569d6253ca81129a90a116dc61ce796131a8ec56
James Samios <james@samios.io>
2018-06-26 17:45:54 +1000

    Game is complete. Story is done. Images and points have been added

--- 2017cyoa/Form1.Designer.cs
+++ 2017cyoa/Form1.Designer.cs
@@ -32,18 +32,23 @@
             this.btn1 = new System.Windows.Forms.Button();
             this.btn2 = new System.Windows.Forms.Button();
             this.btn3 = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.pointsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // lblStory
             // 
+            this.lblStory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblStory.Location = new System.Drawing.Point(12, 9);
             this.lblStory.Name = "lblStory";
-            this.lblStory.Size = new System.Drawing.Size(389, 163);
+            this.lblStory.Size = new System.Drawing.Size(389, 136);
             this.lblStory.TabIndex = 0;
             this.lblStory.Text = "label1";
             // 
             // btn1
             // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn1.Location = new System.Drawing.Point(12, 326);
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(386, 96);
@@ -55,6 +60,7 @@
             // 
             // btn2
             // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn2.Location = new System.Drawing.Point(12, 438);
             this.btn2.Name = "btn2";
             this.btn2.Size = new System.Drawing.Size(386, 93);
@@ -65,6 +71,7 @@
             // 
             // btn3
             // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn3.Location = new System.Drawing.Point(12, 549);
             this.btn3.Name = "btn3";
             this.btn3.Size = new System.Drawing.Size(386, 96);
@@ -73,11 +80,31 @@
             this.btn3.TextAlign = System.Drawing.ContentAlignment.TopLeft;
             this.btn3.UseVisualStyleBackColor = true;
             // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 148);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(386, 172);
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // pointsLabel
+            // 
+            this.pointsLabel.AutoSize = true;
+            this.pointsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pointsLabel.Location = new System.Drawing.Point(337, 129);
+            this.pointsLabel.Name = "pointsLabel";
+            this.pointsLabel.Size = new System.Drawing.Size(58, 16);
+            this.pointsLabel.TabIndex = 5;
+            this.pointsLabel.Text = "Points: 0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(412, 657);
+            this.Controls.Add(this.pointsLabel);
+            this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.btn3);
             this.Controls.Add(this.btn2);
             this.Controls.Add(this.btn1);
@@ -85,7 +112,9 @@
             this.Name = "Form1";
             this.Text = "CYOA: Ethical Edition";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -95,6 +124,8 @@
         private System.Windows.Forms.Button btn1;
         private System.Windows.Forms.Button btn2;
         private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label pointsLabel;
     }
 }
 
--- 2017cyoa/Form1.cs
+++ 2017cyoa/Form1.cs
@@ -21,16 +21,22 @@ namespace _2017cyoa
             public string story;
             public int opt1;
             public string opt1Text;
+            public int opt1Points;
             public int opt2;
             public string opt2Text;
+            public int opt2Points;
             public int opt3;
             public string opt3Text;
+            public int opt3Points;
+            public string img;
         }
 
         // Create an array of our new data type.
         // remember to change the value in the [] 
         // to the number of lines in your text file
-        Room[] room = new Room[20]; 
+        Room[] room = new Room[40];
+        int score = new int();
+        int currentRoom = new int();
 
         public Form1()
         {
@@ -72,10 +78,14 @@ namespace _2017cyoa
                 room[counter].story = tmp[1];
                 room[counter].opt1 = int.Parse(tmp[2]);
                 room[counter].opt1Text = tmp[3];
-                room[counter].opt2 = int.Parse(tmp[4]);
-                room[counter].opt2Text = tmp[5];
-                room[counter].opt3 = int.Parse(tmp[6]);
-                room[counter].opt3Text = tmp[7];
+                room[counter].opt1Points = int.Parse(tmp[4]);
+                room[counter].opt2 = int.Parse(tmp[5]);
+                room[counter].opt2Text = tmp[6];
+                room[counter].opt2Points = int.Parse(tmp[7]);
+                room[counter].opt3 = int.Parse(tmp[8]);
+                room[counter].opt3Text = tmp[9];
+                room[counter].opt3Points = int.Parse(tmp[10]);
+                room[counter].img = tmp[11];
                 counter++;
             }
 
@@ -86,7 +96,9 @@ namespace _2017cyoa
         // of rooms into the controls on the screen.
         private void loadRoom(int roomID)
         {
+            currentRoom = roomID;
             lblStory.Text = room[roomID].story;
+            pictureBox.Image = Image.FromFile(@"img\" + room[roomID].img);
             btn1.Text = room[roomID].opt1Text;
 
             // store the target room for the button click
@@ -103,6 +115,33 @@ namespace _2017cyoa
         {
             // Cast the 'sender' as a button
             Button sentButton = (Button)sender;
+
+            if (sentButton == btn1)
+            {
+                score += room[currentRoom].opt1Points;
+            }
+            else if (sentButton == btn2)
+            {
+                score += room[currentRoom].opt2Points;
+            }
+            else if (sentButton == btn3)
+            {
+                score += room[currentRoom].opt3Points;
+            }
+            if (sentButton == btn1 && btn1.Text == "START AGAIN")
+            {
+                score = 0;
+            }
+            else if (sentButton == btn2 && btn2.Text == "START AGAIN")
+            {
+                score = 0;
+            }
+            else if (sentButton == btn3 && btn3.Text == "START AGAIN")
+            {
+                score = 0;
+            }
+            pointsLabel.Text = "Points: " + score.ToString();
+                
 
             // Extract the target room of the button
             int ID = int.Parse(sentButton.Tag.ToString());
